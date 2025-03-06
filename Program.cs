8d3b355467c1d49238d34bb6b8baeda45074d05b
Mirzoaliev Bahodur <bahodurmirzoaliev2008@gmail.com>
2025-03-06 21:15:55 +0500

    task2

--- Program.cs
+++ Program.cs
@@ -1,30 +1,40 @@
-﻿using System;
+﻿//task1
+// using System;
 
-class Program
-{
-    static void Main()
-    {
-        string input = Console.ReadLine();
-        double deposit = Convert.ToDouble(input);
+// class Program
+// {
+//     static void Main()
+//     {
+//         string input = Console.ReadLine();
+//         double deposit = Convert.ToDouble(input);
 
-        double interestRate = 0;
+//         double interestRate = 0;
 
-        if (deposit < 100)
-        {
-            interestRate = 0.05;
-        }
-        else if (deposit <= 200)
-        {
-            interestRate = 0.07;
-        }
-        else
-        {
-            interestRate = 0.10;
-        }
+//         if (deposit < 100)
+//         {
+//             interestRate = 0.05;
+//         }
+//         else if (deposit <= 200)
+//         {
+//             interestRate = 0.07;
+//         }
+//         else
+//         {
+//             interestRate = 0.10;
+//         }
 
-        double interest = deposit * interestRate;
-        double totalAmount = deposit + interest;
+//         double interest = deposit * interestRate;
+//         double totalAmount = deposit + interest;
  
-        Console.WriteLine(totalAmount);
-    }
+//         Console.WriteLine(totalAmount);
+//     }
+// }
+
+//task2 
+int n = int.Parse(System.Console.ReadLine());
+int sum = 0, z = 1;
+for(int i=n; i>0; i/=10)
+{
+    z *= i%10;
 }
+System.Console.WriteLine("1*2*3*4*5 = " + z);
