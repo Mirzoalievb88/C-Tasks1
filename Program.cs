5dbf354b565418d21c240232f8f84001658bf964
Mirzoaliev Bahodur <bahodurmirzoaliev2008@gmail.com>
2025-03-06 20:56:42 +0500

    first task

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        double deposit = Convert.ToDouble(input);
+
+        double interestRate = 0;
+
+        if (deposit < 100)
+        {
+            interestRate = 0.05;
+        }
+        else if (deposit <= 200)
+        {
+            interestRate = 0.07;
+        }
+        else
+        {
+            interestRate = 0.10;
+        }
+
+        double interest = deposit * interestRate;
+        double totalAmount = deposit + interest;
+ 
+        Console.WriteLine(totalAmount);
+    }
+}
